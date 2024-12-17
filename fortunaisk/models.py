e144a9d5937278c04b6f881230a9d6d9d3ea5bac
erkaek <erkaekanon@outlook.com>
2024-12-17 21:01:29 +0100

    a a#

--- fortunaisk/models.py
+++ fortunaisk/models.py
@@ -107,3 +107,15 @@ class FortunaISKSettings(models.Model):
 
     class Meta:
         ordering = ["-next_drawing_date"]
+
+def get_default_lottery():
+    """Return the ID of the default lottery, creating one if it doesn't exist."""
+    lottery, _ = Lottery.objects.get_or_create(
+        lottery_reference="DEFAULT-LOTTERY",
+        defaults={
+            "ticket_price": 10_000_000,
+            "start_date": timezone.now(),
+            "end_date": timezone.now() + timezone.timedelta(days=30),
+        },
+    )
+    return lottery.id
