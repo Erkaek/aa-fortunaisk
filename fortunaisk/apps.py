30061c321ee7cc52a3ea853d710497b78140cfa5
erkaek <erkaekanon@outlook.com>
2024-12-19 09:07:05 +0100

    Refactor task setup in apps.py and tasks.py; improve logging format and connect setup_tasks to post-migrate signal.

--- fortunaisk/apps.py
+++ fortunaisk/apps.py
@@ -1,9 +1,14 @@
+# Django
 from django.apps import AppConfig
 
+
 class FortunaiskConfig(AppConfig):
-    name = 'fortunaisk'
+    name = "fortunaisk"
 
     def ready(self):
-        from .tasks import setup_tasks
+        # Django
         from django.db.models.signals import post_migrate
+
+        from .tasks import setup_tasks
+
         post_migrate.connect(setup_tasks, sender=self)
