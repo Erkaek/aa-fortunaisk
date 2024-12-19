6f37a1c7f18702673c8ffe64719e79d0b77031f0
erkaek <erkaekanon@outlook.com>
2024-12-19 02:40:50 +0100

    Connect setup_tasks to post-migrate signal in FortunaiskConfig; remove redundant setup call.

--- fortunaisk/apps.py
+++ fortunaisk/apps.py
@@ -5,9 +5,8 @@ from django.db.models.signals import post_migrate
 
 class FortunaiskConfig(AppConfig):
     name = "fortunaisk"
-    verbose_name = "Fortunaisk Lottery System"
 
     def ready(self):
-        from .tasks import setup_tasks  # Assurez-vous que cette ligne est correcte
+        from .tasks import setup_tasks
 
-        setup_tasks()
+        post_migrate.connect(setup_tasks, sender=self)
