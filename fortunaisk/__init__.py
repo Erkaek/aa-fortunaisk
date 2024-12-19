a1d89835a7cb0b5cb79878efbf799813b6e96b76
erkaek <erkaekanon@outlook.com>
2024-12-19 09:19:43 +0100

    Remove unnecessary comments and simplify app initialization in __init__.py

--- fortunaisk/__init__.py
+++ fortunaisk/__init__.py
@@ -1,7 +1,4 @@
-# This will make sure the app is always imported when
-# Django starts so that shared_task will use this app.
-from .celery import app as celery_app
+"""Initialize the app"""
 
-__all__ = ("celery_app",)
 __version__ = "0.0.1"
 __title__ = "fortunaisk"
