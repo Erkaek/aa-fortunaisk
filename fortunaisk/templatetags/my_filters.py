d03d890c9f025ffb3e32ba857d89172fdbbf9f23
erkaek <erkaekanon@outlook.com>
2024-12-22 12:24:53 +0100

    Add custom template filter for safe index access; load my_filters in lottery_form.html

---
+++ fortunaisk/templatetags/my_filters.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def index(sequence, position):
+    """
+    Retourne sequence[position] si possible, sinon chaîne vide.
+    """
+    try:
+        return sequence[position]
+    except (IndexError, TypeError, ValueError):
+        return ""
