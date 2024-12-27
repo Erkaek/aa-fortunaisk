f5370c53154cc46758c5c5107692c9012f54aa09
Erkaek <erkaekanon@outlook.com>
2024-12-28 00:35:58 +0100

    Fix migration file naming and update dependencies for periodic tasks setup

--- fortunaisk/migrations/0002_setup_periodic_tasks.py
+++ fortunaisk/migrations/0002_setup_periodic_tasks.py
@@ -1,5 +1,6 @@
-# fortunaisk/migrations/000X_setup_periodic_tasks.py
+# fortunaisk/migrations/0002_setup_periodic_tasks.py
 
+# Django
 from django.db import migrations
 
 
@@ -7,14 +8,16 @@ def setup_periodic_tasks_func(apps, schema_editor):
     """
     Exécute la fonction de configuration des tâches périodiques.
     """
+    # fortunaisk
     from fortunaisk.tasks import setup_periodic_tasks
+
     setup_periodic_tasks()
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('fortunaisk', '0001_initial.py'),  # Remplacez par le nom de votre dernière migration
+        ("fortunaisk", "0001_initial"),
     ]
 
     operations = [
