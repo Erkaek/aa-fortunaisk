517bb67927d2d5d091c37d786ee4abee5ea03234
Erkaek <erkaekanon@outlook.com>
2024-12-28 14:01:21 +0100

    Remove HTML comments from lottery form templates and add a new split filter for string manipulation in templates

--- fortunaisk/templatetags/my_filters.py
+++ fortunaisk/templatetags/my_filters.py
@@ -6,10 +6,22 @@ from django import template
 register = template.Library()
 
 
-@register.filter
+@register.filter(name="index")
 def index(sequence, position):
     """Returns the item at the given position in the sequence."""
     try:
         return sequence[position]
     except (IndexError, TypeError, ValueError):
         return ""
+
+
+@register.filter(name="split")
+def split(value, delimiter):
+    """
+    Divise une chaîne de caractères en une liste basée sur le délimiteur donné.
+    Usage dans le template: {{ value|split:"," }}
+    """
+    try:
+        return value.split(delimiter)
+    except AttributeError:
+        return []
