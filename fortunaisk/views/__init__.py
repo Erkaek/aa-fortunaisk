2de67b016e842f057cc5a677cc789e68432139c0
Erkaek <erkaekanon@outlook.com>
2024-12-28 16:01:24 +0100

    Refactor view imports for improved organization; consolidate view imports into a single module and update __all__ exports accordingly.

--- fortunaisk/views/__init__.py
+++ fortunaisk/views/__init__.py
@@ -1,35 +1,35 @@
 # fortunaisk/views/__init__.py
 
-from .admin_views import admin_dashboard, distribute_prize, resolve_anomaly
-from .autolottery_views import (
-    create_auto_lottery,
+from .views import create_auto_lottery  # Importer la vue renommée
+from .views import (
+    admin_dashboard,
+    create_lottery,
     delete_auto_lottery,
+    distribute_prize,
     edit_auto_lottery,
     list_auto_lotteries,
-)
-from .lottery_views import (
-    create_lottery,
     lottery,
     lottery_detail,
     lottery_history,
     lottery_participants,
+    resolve_anomaly,
     terminate_lottery,
     ticket_purchases,
+    user_dashboard,
     winner_list,
 )
-from .user_views import user_dashboard
 
 __all__ = [
     "admin_dashboard",
-    "resolve_anomaly",
     "distribute_prize",
-    "create_auto_lottery",
+    "resolve_anomaly",
     "list_auto_lotteries",
+    "create_auto_lottery",  # Inclure la vue renommée
     "edit_auto_lottery",
     "delete_auto_lottery",
+    "lottery_detail",
     "create_lottery",
     "lottery",
-    "lottery_detail",
     "lottery_history",
     "lottery_participants",
     "terminate_lottery",
